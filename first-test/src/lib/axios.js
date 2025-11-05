b1db9db1667414f57ab1cb737a0502c083f73a5b
Hasanor Dim <hasanordimasimpan@gmail.com>
2025-11-05 14:00:43 +0800

    3

--- first-test/src/lib/axios.js
+++ first-test/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
+    process.env.NODE_ENV === "development"
       ? "http://localhost:5000/api"
       : "/api",
   withCredentials: true,
